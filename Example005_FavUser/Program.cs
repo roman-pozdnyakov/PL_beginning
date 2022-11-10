063a9b1dcf978682e5b5ea3e77c980e54d603d2f
Roman Pozdnyakov <proklus@gmail.com>
2022-11-10 23:02:00 +0300

    Favourite user greetings added

---
+++ Example005_FavUser/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "прокл")
+{
+    Console.WriteLine("Наконец-то ты вернулся!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
